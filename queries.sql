d83b1b2a4ef3344864149b5ed04b44ad7d60c749
Gayatri Gadhe <121817101+gayatrigadhe123@users.noreply.github.com>
2026-09-18 18:05:16 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
---Display All Employees
+--Q1. Display All Employees
+SELECT * FROM GEMPLOYEES;
 
-SELECT * FROM EMPLOYEES;
+--Q2. Find All employees whose salary greater than 60000
+select * from Gemployees where salary >60000;
+
+--Q3. Find the average Salary of all employees
+select avg(salary) from Gemployees 
+
+--Q4. Find the highest salary from all employees
+select max(salary) from Gemployees 
+
+--Q5. Count employee in IT department
+select count(*) from Gemployees where department='IT'
+
+
+
